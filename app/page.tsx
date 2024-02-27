77e4b9fd566736f0e3385675803e6e8c87b96e58
Tianhao Zhang <zhangth1990@hotmail.com>
2024-02-27 16:59:50 -0700

    add assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
-    return <h1 className="font-5xl text-white">Figma</h1>;
+    return (
+        <div className="h-[100vh] w-full flex justify-center items-center text-center">
+            <h1 className="text-5xl text-white">Figma</h1>
+        </div>
+    );
 }
